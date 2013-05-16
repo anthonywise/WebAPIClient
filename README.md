498a5ff30a86b6ad251fb9628d889f2529b84ee2
Ben Bowen <bbowen@tradestation.com>
2013-05-16 17:32:43 -0400

    Added NuGet references

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Client library for the TradeStation WebAPI
 
 History: 
 
+05/16/2013	Added NuGet references
 05/15/2013	Initial checkin of project.
--- WebAPIClient/Properties/AssemblyInfo.cs
+++ WebAPIClient/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebAPIClient")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Client library for the TradeStation WebAPI")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TradeStation")]
 [assembly: AssemblyProduct("WebAPIClient")]
 [assembly: AssemblyCopyright("Copyright © TradeStation 2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("2012")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
