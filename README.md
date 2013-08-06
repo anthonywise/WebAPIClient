80c3ad35fa79f61c0c5c52dbb3de75edc868e086
Ben Bowen <bbowen@tradestation.com>
2013-08-06 14:49:31 -0400

    Added ApiUtility class with static methods for IsOnline (marketdatanormal) and GetVersion

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Client library for the TradeStation WebAPI
 
 History:   
   
+08/03/2013  Added ApiUtility class with static methods for IsOnline (marketdatanormal) and GetVersion  
+07/26/2013  Fixed bug with listeners where they were not put in a Stopped state in certain cases  
 06/28/2013  Added KeepAliveFailure event  
 06/13/2013  Updated Listener code to use TPL instaed of EAP. Added Symbol to barchart listener. Added IsRunning property to base listener class.  
 06/12/2013  Added UserCredentials to the Client object which is used when requesting a new access token after in response to the AccessTokenExpired event  
---
+++ WebAPIClient/V2/ApiUtility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using TradeStation.SystemTeam.Tools.WebAPI.WebAPIObjects;
+
+
+namespace TradeStation.SystemTeam.Tools.WebAPI.WebAPIClient.V2
+{
+	public static class ApiUtility
+	{
+		private const int UtilityTimeout = 5000;
+		
+
+		public static bool IsOnline(Uri apiRoot)
+		{
+			try
+			{
+				Uri uri = new Uri(apiRoot, "/v2/Status.ashx");
+				WebHeaderCollection headers = new WebHeaderCollection();
+				string result = HttpClient.HttpGet(uri, headers, UtilityTimeout);
+				return result == "marketdatanormal";
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public static bool IsOnline(WebAPIEnvironment environment)
+		{		
+			return IsOnline(WebAPILocation.GetEnvironmentUri(environment));
+		}
+
+		public static int GetVersion(Uri apiRoot)
+		{
+			int version = 0;
+			try
+			{
+				Uri uri = new Uri(apiRoot, "/v2/version.txt");
+				WebHeaderCollection headers = new WebHeaderCollection();
+				string result = HttpClient.HttpGet(uri, headers, UtilityTimeout);
+				string[] fields = result.Split('/');
+				if (fields.Length == 4)
+				{
+					int.TryParse(fields[fields.Length - 1], out version);
+				}
+			}
+			catch
+			{
+				version = -1;
+			}
+
+			return version;
+		}
+
+		public static int GetVersion(WebAPIEnvironment environment)
+		{
+			return GetVersion(WebAPILocation.GetEnvironmentUri(environment));
+		}
+	}
+}
